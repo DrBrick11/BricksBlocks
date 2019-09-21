c7ac05b6a963dc47bca321c3a2a40a222b24f0ad
Brick <DrBrick11@gmail.com>
2019-09-20 22:29:18 -0500

    Updates

---
+++ precache-manifest.0a84170634cf6569b1edca307ea4e117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7925f841acba452a58e60d6fa24a027f",
+    "url": "/BricksBlocks/index.html"
+  },
+  {
+    "revision": "cefa9ed3e37cbde8284c",
+    "url": "/BricksBlocks/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "740cbce19749fba35774",
+    "url": "/BricksBlocks/static/js/2.681b5053.chunk.js"
+  },
+  {
+    "revision": "cefa9ed3e37cbde8284c",
+    "url": "/BricksBlocks/static/js/main.5a0c0da7.chunk.js"
+  },
+  {
+    "revision": "8f941f23d4e472792456",
+    "url": "/BricksBlocks/static/js/runtime-main.883c0b75.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/BricksBlocks/static/media/logo.25bf045c.svg"
+  }
+]);
